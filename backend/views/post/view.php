aacdced0b9f7ca9db82b0e8919e1f8feee812cc4
aragakiyui <heng8050lee@foxmail.com>
2018-04-07 15:33:06 +0800

    1.3 完成文章查看功能

--- backend/views/post/view.php
+++ backend/views/post/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /* @var $model common\models\Post */
 
 $this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => '文章管理', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="post-view">
@@ -15,11 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('修改', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '确定删除该文章？',
                 'method' => 'post',
             ],
         ]) ?>
@@ -32,11 +32,26 @@ $this->params['breadcrumbs'][] = $this->title;
             'title',
             'content:ntext',
             'tags:ntext',
-            'status',
-            'create_time:datetime',
-            'update_time:datetime',
-            'author_id',
+            // 'status',
+            [
+                'attribute' => 'status',
+                'value' => $model->postStatus->name,
+            ],
+            [
+                'attribute' => 'create_time',
+                'value' => date('Y-m-d H:i:s', $model->create_time),
+            ],
+            [
+                'attribute' => 'update_time',
+                'value' => date('Y-m-d H:i:s', $model->update_time),
+            ],
+            [
+                'attribute' => 'author_id',
+                'value' => $model->author->nickname,
+            ],
         ],
+        'template' => '<tr><th style="width:120px;">{label}</th><td>{value}</td></tr>',
+        'options' => ['class' => 'table table-striped table-border detail-view lh'],
     ]) ?>
 
 </div>
