ebf529c396e31144752496b6292b3d2f91a58ddd
aragakiyui <heng8050lee@foxmail.com>
2018-04-07 16:42:55 +0800

    查询构建器 - 帖子修改页

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\Poststatus;
+use common\models\Adminuser;
+//use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
@@ -18,16 +21,59 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?php
+        // 第一种方法 - ActiveRecord
+
+        /*
+        $allStatusObj = Poststatus::find()->all();
+        $allStatus = ArrayHelper::map($allStatusObj, 'id', 'name');
+        */
+
+        // 第二种方法 - Command
+        /*
+        $postArr = Yii::$app->db->createCommand('SELECT id,name FROM poststatus')->queryAll();
+        $allStatus = ArrayHelper::map($postArr, 'id', 'name');
+        */
+
+        // 第三种方法 - QueryBuild
+        /*
+        $allStatus = (new \yii\db\Query())
+            ->select(['name', 'id'])
+            ->from('poststatus')
+            ->indexBy('id')
+            ->column();
+        */
+
+        // 第四种 - ActiveRecord + QueryBuild
+        /*
+        $allStatus = Poststatus::find()
+        ->select(['name', 'id'])
+        ->indexBy('id')
+        ->column();
+        */
+    ?>
+
+    <?= $form->field($model, 'status')->dropDownList(
+            Poststatus::find()
+            ->select(['name', 'id'])
+            ->indexBy('id')
+            ->column(),
+        ['prompt' => '请选择状态']) ?>
 
     <?= $form->field($model, 'create_time')->textInput() ?>
 
     <?= $form->field($model, 'update_time')->textInput() ?>
 
-    <?= $form->field($model, 'author_id')->textInput() ?>
+    <?= $form->field($model, 'author_id')->dropDownList(
+            Adminuser::find()
+            ->select(['nickname', 'id'])
+            ->indexBy('id')
+            ->column(),
+            ['prompt' => '请选择文章作者']
+    ) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('保存', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
